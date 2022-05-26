391081412ccd7d0ee69e02e90cd64edbf8567a8d
D <2437757398@qq.com>
2022-05-26 09:45:47 +0800

    tabbar开发完成

---
+++ pages/shoppingCat/shoppingCat.vue
@@ -0,0 +1,19 @@
+<template>
+	<view>
+		shoppingCat
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
