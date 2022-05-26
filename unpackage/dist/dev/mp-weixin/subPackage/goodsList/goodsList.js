896a1478212b2120056049ebf62d174404952291
D <2437757398@qq.com>
2022-05-26 15:51:19 +0800

    合并首页

---
+++ unpackage/dist/dev/mp-weixin/subPackage/goodsList/goodsList.js
@@ -0,0 +1,31 @@
+(global["webpackJsonp"] = global["webpackJsonp"] || []).push([["subPackage/goodsList/goodsList"],{
+
+/***/ 51:
+/*!*****************************************************************************************************!*\
+  !*** C:/Users/Administrator/Desktop/Mall2018/main.js?{"page":"subPackage%2FgoodsList%2FgoodsList"} ***!
+  \*****************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+/* WEBPACK VAR INJECTION */(function(createPage) {__webpack_require__(/*! uni-pages */ 5);
+var _vue = _interopRequireDefault(__webpack_require__(/*! vue */ 3));
+var _goodsList = _interopRequireDefault(__webpack_require__(/*! ./subPackage/goodsList/goodsList.vue */ 52));function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}wx.__webpack_require_UNI_MP_PLUGIN__ = __webpack_require__;
+createPage(_goodsList.default);
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! ./node_modules/@dcloudio/uni-mp-weixin/dist/index.js */ 1)["createPage"]))
+
+/***/ }),
+
+/***/ 52:
+/*!**********************************************************************************!*\
+  !*** C:/Users/Administrator/Desktop/Mall2018/subPackage/goodsList/goodsList.vue ***!
+  \**********************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed (from ./node_modules/@dcloudio/webpack-uni-mp-loader/lib/style.js):\nError: ENOENT: no such file or directory, open 'C:\\Users\\Administrator\\Desktop\\Mall2018\\subPackage\\goodsList\\goodsList.vue'");
+
+/***/ })
+
+},[[51,"common/runtime","common/vendor"]]]);
+//# sourceMappingURL=../../../.sourcemap/mp-weixin/subPackage/goodsList/goodsList.js.map
